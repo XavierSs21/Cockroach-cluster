5ad477ea3d7dd33f12c6a47d0af98e763115d85b
Xavier Sotomayor <xavier.sotomayor21@hotmail.com>
2026-05-28 23:30:53 -0600

    initial project structure

---
+++ backend/src/main/java/com/cockroach/crdbcluster/CrdbclusterApplication.java
@@ -0,0 +1,13 @@
+package com.cockroach.crdbcluster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrdbclusterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrdbclusterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cockroach/crdbcluster/CrdbclusterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cockroach.crdbcluster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrdbclusterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
